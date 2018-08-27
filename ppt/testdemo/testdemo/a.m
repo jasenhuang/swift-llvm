c145c114879ce23bb8a46ecde331a113b40bdfc8
jasenhuang(黄栩生) <jasenhuang@tencent.com>
2018-08-27 16:47:30 +0800

    moidfy

--- ppt/testdemo/testdemo/a.m
+++ ppt/testdemo/testdemo/a.m
@@ -20,8 +20,12 @@ void say(NSString* msg)// __attribute__((annotate("my annotation")))
     NSLog(@"Hello Instance %@", msg);
     dispatch_async(dispatch_get_main_queue(), ^{
         NSLog(@"calling block");
-        [self sayInstanceHello:msg to:user];
+        //[self sayInstanceHello:msg to:user];
     });
+    [self controlFlowFlattening];
+    [self controlFlowFlattening1];
+    [self controlFlowFlattening2];
+    [self controlFlowFlattening3];
 }
 
 - (void)controlFlowFlattening __attribute__((annotate("obfuscate")))
@@ -34,6 +38,162 @@ void say(NSString* msg)// __attribute__((annotate("my annotation")))
         [array addObject:@(i)];
     }
     NSLog(@"time = %@", @(CFAbsoluteTimeGetCurrent() - begin));
+}
+
+// obfuscate (1)
+- (void)controlFlowFlattening1 __attribute__((annotate("obfuscate")))
+{
+    CFAbsoluteTime begin = CFAbsoluteTimeGetCurrent();
+    NSMutableArray * array = [NSMutableArray array];
+    int i = 0 ;
+    int swVar1 = 1;
+    while(swVar1 != 0){
+        switch (swVar1){
+            case 1:
+                i = 1;
+                break;
+            case 2:
+            {
+                if (i < 100000){
+                    swVar1 = 3;
+                }else {
+                    swVar1 = 0;
+                }
+                break;
+            }
+            case 3:
+            {
+                [array addObject:@(i++)];
+                swVar1 = 2;
+                break;
+            }
+        }
+    }
+    NSLog(@"time 1 = %@", @(CFAbsoluteTimeGetCurrent() - begin));
+}
+
+// obfuscate (2)
+- (void)controlFlowFlattening2 __attribute__((annotate("obfuscate")))
+{
+    CFAbsoluteTime begin = CFAbsoluteTimeGetCurrent();
+    NSMutableArray * array = [NSMutableArray array];
+    int i = 0 ;
+    int swVar1 = 1;
+    while(swVar1 != 0){
+        switch (swVar1){
+            case 1:
+                i = 1;
+                break;
+            case 2:
+            {
+                int swVar2 = 1;
+                while(swVar2 != 0){
+                    switch(swVar2){
+                        case 1:
+                        {
+                            if (i < 100000){
+                                swVar2 = 2;
+                            }else {
+                                swVar2 = 3;
+                            }
+                            break;
+                        }
+                        case 2:
+                        {
+                            swVar1 = 3;
+                            swVar2 = 0;
+                            break;
+                        }
+                        case 3:
+                        {
+                            swVar1 = 0;
+                            swVar2 = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+            case 3:
+            {
+                [array addObject:@(i++)];
+                swVar1 = 2;
+                break;
+            }
+        }
+    }
+    NSLog(@"time 2 = %@", @(CFAbsoluteTimeGetCurrent() - begin));
+}
+
+// obfuscate (3)
+- (void)controlFlowFlattening3 __attribute__((annotate("obfuscate")))
+{
+    CFAbsoluteTime begin = CFAbsoluteTimeGetCurrent();
+    NSMutableArray * array = [NSMutableArray array];
+    int i = 0 ;
+    int swVar1 = 1;
+    while(swVar1 != 0){
+        switch (swVar1){
+            case 1:
+                i = 1;
+                break;
+            case 2:
+            {
+                int swVar2 = 1;
+                while(swVar2 != 0){
+                    switch(swVar2){
+                        case 1:
+                        {
+                            int swVar3 = 1;
+                            while(swVar3 != 0){
+                                switch(swVar3){
+                                    case 1:
+                                    {
+                                        if (i < 100000){
+                                            swVar3 = 2;
+                                        }else {
+                                            swVar3 = 3;
+                                        }
+                                        break;
+                                    }
+                                    case 2:
+                                    {
+                                        swVar2 = 2;
+                                        swVar3 = 0;
+                                        break;
+                                    }
+                                    case 3:
+                                    {
+                                        swVar2 = 3;
+                                        swVar3 = 0;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        case 2:
+                        {
+                            swVar1 = 3;
+                            swVar2 = 0;
+                            break;
+                        }
+                        case 3:
+                        {
+                            swVar1 = 0;
+                            swVar2 = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+            case 3:
+            {
+                [array addObject:@(i++)];
+                swVar1 = 2;
+                break;
+            }
+        }
+    }
+    NSLog(@"time 3 = %@", @(CFAbsoluteTimeGetCurrent() - begin));
 }
 
 + (NSData *)encrypt:(NSData *)plainData key:(NSData *)key iv:(const void *)iv {
